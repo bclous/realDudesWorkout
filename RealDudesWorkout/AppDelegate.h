fdb936279702765f9c3787a9ec7a43d8d71f09db
bclous <brian.clouser@gmail.com>
2016-03-09 19:49:43 -0500

    Initial Commit

---
+++ RealDudesWorkout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealDudesWorkout
+//
+//  Created by Brian Clouser on 3/9/16.
+//  Copyright © 2016 The Qwiz LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealDudesWorkout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealDudesWorkout
+//
+//  Created by Brian Clouser on 3/9/16.
+//  Copyright © 2016 The Qwiz LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
