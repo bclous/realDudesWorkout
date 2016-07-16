c0567bcbacafa678f1004df944cef1c1d3ace155
bclous <brian.clouser@gmail.com>
2016-07-15 20:25:53 -0400

    changed top cell looks good

---
+++ RealDudesWorkout/WorkoutOnBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  WorkoutOnBoardViewController.h
+//  RealDudesWorkout
+//
+//  Created by Brian Clouser on 7/15/16.
+//  Copyright © 2016 The Qwiz LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkoutOnBoardViewController : UIViewController
+
+@end
